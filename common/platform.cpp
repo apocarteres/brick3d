963909de84aa233296528d0d7f877b6599bca074
Alexander Paderin <apaderin@andiks.com>
2015-08-30 08:41:02 +0300

    initial commit

---
+++ common/platform.cpp
@@ -0,0 +1,6 @@
+#include "platform.hpp"
+#include <string>
+
+std::string ResourcePath(std::string fileName) {
+	return "/Users/sandro/06_diffuse_lighting/resources/" + fileName;
+}
---
+++ start_osx.sh
@@ -0,0 +1,3 @@
+#/bin/sh -e
+
+DYLD_LIBRARY_PATH=./lib ./bin/main
